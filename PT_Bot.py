4bce90f52bddc8ab04fd8b6e7b2f02c22b7fcc5f
Actually_Mesfin <mesfinalemayehu435@gmail.com>
2022-06-18 17:42:02 +0300

    Changed a few coordinates

--- PT_Bot.py
+++ PT_Bot.py
@@ -5,12 +5,6 @@ import keyboard
 import random
 import win32api, win32con
 
-#Tile 1 Pos X:  419 Y:  500 RGB: (184, 188, 235)
-#Tile 2 PosX:  551 Y:  500 RGB: (180, 183, 235)
-#Tile 3 PosX:  693 Y:  500 RGB: (180, 183, 235)
-#Tile 4 PosX:  781 Y:  500 RGB: (164, 169, 221)
-
-
 def click(x, y):
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
@@ -19,12 +13,12 @@ def click(x, y):
 
 while keyboard.is_pressed('q') == False:
 
-    if pyautogui.pixel(419, 500)[0] == 0:
-        click(419, 500)
-    if pyautogui.pixel(581, 500)[0] == 0:
-        click(551, 500)
-    if pyautogui.pixel(581, 500)[0] == 0:
-        click(693, 500)
-    if pyautogui.pixel(581, 500)[0] == 0:
-        click(781, 500)
+    if pyautogui.pixel(403, 500)[0] == 0:
+        click(403, 500)
+    if pyautogui.pixel(557, 500)[0] == 0:
+        click(557, 500)
+    if pyautogui.pixel(770, 500)[0] == 0:
+        click(675, 500)
+    if pyautogui.pixel(869, 500)[0] == 0:
+        click(869, 500)
     
